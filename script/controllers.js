6e10f6201842c6b4fc0f13849441e9f2ed139502
Michael Bauer <mihi@lo-res.org>
2014-08-16 23:33:43 +0200

    large progress added more icons and support for multiple activities

--- script/controllers.js
+++ script/controllers.js
@@ -1,38 +1,43 @@
 var actispyControllers = angular.module('actispyControllers',[]);
 
+// menu controller
 actispyControllers.controller('MenuCtrl', ['$scope' , function($scope) {
     
     }])
 
+// new activity controller
 actispyControllers.controller('NewActivityCtrl', ['$scope', function($scope) {
-    $scope.hours= "00";
-    $scope.minutes = "00";
-    $scope.seconds = "00";
+    $scope.timer = "00:00:00";
     $scope.action = "Wait";
     $scope.activity = {};
     $scope.activity.distance = 0;
     $scope.activity.points = [];
     $scope.buttonstatus = "disabled";
+    $scope.activity.pace = "NA";
+    $scope.activity.averagepace = "NA";
+    $scope.activity.type = 'running';
 
     // set up leaflet.
     var map = L.map('map')
     L.tileLayer('http://{s}.tiles.mapbox.com/v3/mihi-tr.j8ec7a21/{z}/{x}/{y}.png', {
-        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery &copy; <a href="http://mapbox.com">Mapbox</a>',
+        attribution: 'Map data &copy; OpenStreetMap, CC-BY-SA, Imagery &copy; Mapbox',
             maxZoom: 18
             }).addTo(map);
 
+    // watch for sattelite accuracy
     $scope.iwatchid = navigator.geolocation.watchPosition(function(p) {
         var ma = parseInt(localStorage.getItem("min-accuracy")) | 10;
-        $scope.accuracy = p.coords.accuracy;
+        $scope.accuracy = Math.round(p.coords.accuracy);
         if ( p.coords.accuracy <= ma ) {
             $scope.action = "Start"; 
             $scope.buttonstatus = "success";
             }
-        $scope.lastposition = p.coords;    
+        $scope.lastposition = L.latLng(p.coords.latitude, p.coords.longitude,
+            p.coords.altitude);    
 
         // set map view to current position 
 
-        map.setView([p.coords.latitude, p.coords.longitude], 15);
+        map.setView([p.coords.latitude, p.coords.longitude], 16);
         if (! $scope.marker) {
             var icon = L.icon( {
                 iconUrl: 'images/marker.png',
@@ -50,41 +55,74 @@ actispyControllers.controller('NewActivityCtrl', ['$scope', function($scope) {
 
         $scope.$apply();    
         }, function() {}, {enableHighAccuracy: true});
+    
+    // the start button action!
     $scope.toggleStart = function() {
         // Don't run if sattelite accuracy is not enough.
         if ( $scope.action == "Wait" ) {
             return null;
             };
 
-        $scope.activity.starttime = new Date().getTime();
         if ( $scope.action == "Start" ) {
+            $scope.activity.starttime = new Date().getTime();
+            $scope.activity.startposition = [$scope.lastposition.lat,
+                $scope.lastposition.lon];
+            var icon = L.icon( {
+                iconUrl: 'images/start-marker.png',
+                iconSize: [10, 10],
+                iconAnchor: [5,5]});
+
+            $scope.lastime = $scope.activity.starttime;
             navigator.geolocation.clearWatch($scope.iwatchid);
             $scope.action = "Stop";
             $scope.buttonstatus = "warning"; 
-            $scope.watchid = navigator.geolocation.watchPosition(function(p) {
-                var dt=new Date(new Date() - $scope.activity.starttime);
-                $scope.hours = dt.getUTCHours();
-                $scope.minutes = dt.getUTCMinutes();
-                $scope.seconds = dt.getUTCSeconds();
 
+            $scope.line = L.polyline([$scope.lastposition], {color: "#000080",
+                    opacity: 0.8}).addTo(map);
+            $scope.startmarker = L.marker($scope.lastposition,
+                {icon: icon}).addTo(map)
+            // the uppdater - watches the position and updates!
+            $scope.watchid = navigator.geolocation.watchPosition(function(p) {
+                var time=new Date().getTime();
+                var dt=new Date(time - $scope.activity.starttime);
+                $scope.timer = dt.toUTCString().split(" ")[4]
+                
+                var cp = L.latLng(p.coords.latitude,    p.coords.longitude,
+                    p.coords.altitude);
                 if ($scope.lastposition) {
-                    $scope.activity.distance += dist($scope.lastposition,
-                        p.coords);
+                        var d = $scope.lastposition.distanceTo(cp);
+                        $scope.activity.distance += d/1000.0;
+                        $scope.activity.pace = (time - $scope.lasttime) / 
+                            (d * 60.0);
+                        $scope.activity.averagepace = dt.getTime() / 
+                            (60 * 1000 * $scope.activity.distance) ;
                     }
                     $scope.activity.points.push(p);
-                    $scope.lastposition = p.coords;
+                    $scope.marker.setLatLng(cp);
+                    $scope.line.addLatLng(cp);
+                    map.setView(cp);
+                    $scope.lastposition = cp;
+                    $scope.lasttime = time;
                     $scope.$apply();
                 }, function() {}, {enableHighAccuracy : true});
             }
+
         else {
             $scope.action = "Start";
             navigator.geolocation.clearWatch($scope.watchid);
+            $scope.activity.endtime=new Date().getTime();
+            $scope.activity.endposition=[$scope.lastposition.lat,
+                $scope.lastposition.lon];
             }
-        console.log("start toggle");
         };
-
+    
+    // Go back to the menu
     $scope.menu = function() {
         window.location.hash = "#/menu";
         }
 
+    // set activity
+    $scope.setActivity = function(type) {
+        $scope.activity.type=type;
+        }
     }])
